c35e8627076ba23426d589223966f16f31657e0f
Camilo Rivera <camilorivera@camilorivera.co>
2023-11-27 17:11:25 -0500

    Camilo Commit

---
+++ temp-legacy-code/src/main/java/com/ivy/legacy/Constants.kt
@@ -0,0 +1,27 @@
+package com.ivy.legacy
+
+object Constants {
+    const val URL_TC =
+        "https://github.com/JCamiloRiveraP"
+    const val URL_PRIVACY_POLICY =
+        "https://github.com/JCamiloRiveraP"
+
+    const val URL_IVY_WALLET_REPO = "https://github.com/JCamiloRiveraP"
+
+    const val URL_ROADMAP = "https://github.com/JCamiloRiveraP"
+
+    const val URL_HELP_CENTER = "https://www.instagram.com/camilorivera.co/"
+
+    const val URL_IVY_TELEGRAM_INVITE = "#"
+
+    const val URL_IVY_WALLET_GOOGLE_PLAY =
+        "#"
+
+    const val CATEGORY_UNSPECIFIED_NAME = "Unspecified"
+
+    const val URL_IVY_CONTRIBUTORS =
+        "https://github.com/JCamiloRiveraP"
+
+    const val URL_GITHUB_SPONSORS =
+        "https://github.com/JCamiloRiveraP"
+}
